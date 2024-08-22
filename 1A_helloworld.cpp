7ecd92f22967aeac3832b8d863a9460c2aa1b3e4
mukeshrothe13 <rothemukesh7@gmail.com>
2024-08-22 20:06:50 +0530

    1A_helloworld.cpp

--- 1A_helloworld.cpp
+++ 1A_helloworld.cpp
@@ -1,7 +1,13 @@
+//Mukesh Rothe
+//23070123089
+//CDS EXP1
 #include<iostream>
 using namespace std;
 int main()
 {
-    cout <<"Hello World!";
+    string a;
+    cout << "Enter your name ";
+    getline(cin,a);
+    cout<<"Hello  " <<a;
     return 0;
 }
